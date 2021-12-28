b560385ced680db266cead657498fe8766db26fe
911-Ciurcau-Leonardo-Iulian <91748424+911-Ciurcau-Leonardo-Iulian@users.noreply.github.com>
2021-12-28 16:34:04 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+/* 
+    from ReadFirst.asm 
+    reads n > 0, the number of strings to be read (including the first string), and the first string s
+    if n is <= 0, the function returns -1, otherwise 0
+*/
+int ReadFirst(int* n, char* s);
+
+/*
+    from Substring.asm
+    returns 1 if s is a substring of buff, otherwise 0
+*/
+int Substring(char* buff, char* s);
+
+/*
+    from Response.asm
+    returns the address of the string to be printed, "Yes\n" if Substring(buff, s) is 1, "No\n" otherwise
+*/
+char* Response(int);
+
+int main()
+{
+    int n;
+    char s[101];
+    scanf("%d", &n);
+    scanf("%s", s);
+    /*1st function*/
+    // if (ReadFirst(&n, s) == -1) 
+    //    return 0;
+    /*end*/
+    char buff[101];
+    for (int i = 2; i <= n; i++)
+    {
+        scanf("%s", buff);
+        /*
+            printf(Response(Substring(buff, s));
+        */
+        printf(strstr(buff, s) ? "Yes\n" : "No\n");
+    }
+
+    return 0;
+}
